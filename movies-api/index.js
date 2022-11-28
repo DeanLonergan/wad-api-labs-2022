f800d1087b0c26edfec0406c0eb5ceaf3d8e0c90
Dean Lonergan <74914758+DeanLonergan@users.noreply.github.com>
2022-11-28 00:29:56 +0000

    added routing for movies and movie reviews.

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import uniqid from 'uniqid';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
 
