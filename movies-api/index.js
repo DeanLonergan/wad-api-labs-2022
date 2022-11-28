9d28526d38cbd1cfab7cba8f4cf8af61d71f30e8
Dean Lonergan <74914758+DeanLonergan@users.noreply.github.com>
2022-11-28 00:41:17 +0000

    Exercises.

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
-import uniqid from 'uniqid';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -12,6 +12,8 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
+
+app.use('/api/genres', genresRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
