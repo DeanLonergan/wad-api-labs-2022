3996e7914280011a786d3040cd42c849d4cb06b5
Dean Lonergan <74914758+DeanLonergan@users.noreply.github.com>
2022-11-30 20:17:53 +0000

    Topic 11 - I've completed all my labs!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
 router.get('/', asyncHandler(async (req, res) => {
@@ -61,6 +62,13 @@ router.post('/:id/reviews', (req, res) => {
             status_code: 404
         });
     }
-  });
+});
+
+//Get upcoming movies from tmdb
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
 
 export default router;
